9b73cde97fb5f507259870ef31fe00148b45c417
hunnypotpy <sarahdraws@gmail.com>
2022-02-15 11:45:05 -0800

    Stepping out code process

---
+++ main.py
@@ -0,0 +1,14 @@
+#import
+
+#nagivate to folder
+
+#determine type of file to be looking for
+
+#if (.jpg) in folder
+#print (file is here!)
+
+#else no (.jpg)
+#print (file is missing!)
+
+#check folder again
+#determine time interval
