a32894987ed20f0e9c05fd7f321de03244b22b00
hunnypotpy <sarahdraws@gmail.com>
2022-02-15 19:50:14 -0800

    Added in first code looking for a file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 #import
+import os
 
 #nagivate to folder
 
 #determine type of file to be looking for
-
 #if (.jpg) in folder
 #print (file is here!)
+for root, dirs, files in os.walk(dir_path):
+    for file in files:
+        if file ends with('.jpg')
+            print (root+'/'+str(file))
+        else:
+            print('File not here!')
+
 
 #else no (.jpg)
 #print (file is missing!)
