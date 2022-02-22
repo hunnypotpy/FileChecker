58b268e648231e81bd7e732769b69d3aa63001f4
edg <glutenfreepy@gmail.com>
2022-02-21 23:39:48 -0800

    change code to search for folders instead of files, removed desktop search if no arg provided, added help text

--- main.py
+++ main.py
@@ -8,31 +8,27 @@ USER_DESKTOP_PATH = os.environ['HOME'] + '/Desktop'
 
 def search_for_green_labeled_files(folder_to_search):
     for root, dirs, files in os.walk(folder_to_search, topdown=True):
-        for file in files:
-            file_path = (root + '/' + str(file))
-            file_color = finder_colors.get(file_path)
-            if file_color == 'green':
-                print(file_path)
-                notifications.desktop_popup("Green Label Found", file_path)
+        for item in dirs:
+            obj_path = (root + '/' + str(item))
+            obj_color = finder_colors.get(obj_path)
+            if obj_color == 'green':
+                print(obj_path)
+                notifications.desktop_popup("Green Label Found", obj_path)
 
 
 if __name__ == "__main__":
     n = len(sys.argv)
     if n == 2:
         search_path = sys.argv[1]
-        print("Searching this location: ")
+        print("Searching this location for directories with green label: ")
         print(search_path)
-        print("***********************")
+        print("*********************************************************")
+        print("Found:")
         search_for_green_labeled_files(search_path)
-        print("***********************")
+        print("*********************************************************")
         print("Search complete!")
 
     elif n == 1:
-        print("Searching this location: ")
-        print(USER_DESKTOP_PATH)
-        print("***********************")
-        search_for_green_labeled_files(USER_DESKTOP_PATH)
-        print("***********************")
-        print("Search complete!")
+        print("Please provide a path to search")
     else:
         print("Too many arguments.  If your path has spaces, be sure to wrap it in quotes")
