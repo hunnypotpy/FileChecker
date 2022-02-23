c8bbc6e3252c4cde446b7c77d72fd4393d597a99
edg <glutenfreepy@gmail.com>
2022-02-22 22:26:53 -0800

    add sleep and intervals for checking the folder over a period of time since client cannot use cron

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import sys
+from time import sleep
 import finder_colors
 import notifications
 
 USER_DESKTOP_PATH = os.environ['HOME'] + '/Desktop'
+# check every 30 minutes for 4 hours
+INTERVAL_IN_MINUTES = 30
+NUMBER_OF_INTERVALS = 8
 
 
 def search_for_green_labeled_files(folder_to_search):
@@ -19,14 +23,19 @@ def search_for_green_labeled_files(folder_to_search):
 if __name__ == "__main__":
     n = len(sys.argv)
     if n == 2:
-        search_path = sys.argv[1]
-        print("Searching this location for directories with green label: ")
-        print(search_path)
-        print("*********************************************************")
-        print("Found:")
-        search_for_green_labeled_files(search_path)
-        print("*********************************************************")
-        print("Search complete!")
+        x = NUMBER_OF_INTERVALS
+        while x != 0:
+            search_path = sys.argv[1]
+            print("Searching this location for directories with green label: ")
+            print(search_path)
+            print("*********************************************************")
+            print("Found:")
+            search_for_green_labeled_files(search_path)
+            print("*********************************************************")
+            print("Search complete!")
+            x = x-1
+            print(x)
+            sleep(INTERVAL_IN_MINUTES * 60)
 
     elif n == 1:
         print("Please provide a path to search")
